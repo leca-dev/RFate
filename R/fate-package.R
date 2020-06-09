b87c7f4239c59d2bdf1c94df008f7bb157ab7b46
petitpoil <44815912+petitpoil@users.noreply.github.com>
2020-06-09 18:49:37 +0200

    Add Documentation for fate package and runFate function

---
+++ R/fate-package.R
@@ -0,0 +1,28 @@
+#' @docType package
+#'
+#' @name fate
+#'
+#' @title
+#' \packageTitle{fate}
+#'
+#' @author
+#' \packageAuthor{fate}
+#'
+#' Maintainer: \packageMaintainer{fate}
+#'
+#' @description
+#' \dQuote{FATE-HD is a dynamic landscape vegetation model that simulates
+#'  interactions between plant modelling entities (e.g. species or plant
+#'  functional groups), their population dynamics and dispersal, whilst taking
+#'  into account external drivers such as disturbance regimes, and environmental
+#'  variations. The model is built on past conceptual and technical developments
+#'  (\href{https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/j.1365-2664.2007.01416.x}{Albert et al., 2008};
+#'  \href{https://onlinelibrary.wiley.com/doi/full/10.1111/j.1600-0587.2009.06000.x}{Midgley et al., 2010})
+#'  but has been entirely recoded in C++ and revisited.}
+#' (\href{https://mayagueguen.github.io/FATE-WEBSITE/papers/Boulangeat_2014_GCB.pdf}{Boulangeat, 2014})
+#'
+#' @section fate functions:
+#' \code{\link{runFate}}
+#'
+#' @keywords package internal
+NULL
