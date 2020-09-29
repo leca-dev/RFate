9448f6d063548a845d01bd67aa885547e3bc1986
petitpoil <44815912+petitpoil@users.noreply.github.com>
2020-09-29 13:51:40 +0200

    Comment code

--- src/LogMessage.h
+++ src/LogMessage.h
@@ -1,3 +1,10 @@
+/**
+  @file LogMessage.h
+  @author Guillaume dupouy
+  @version 1.0
+  @date 2020/09/15
+*/
+
 #ifndef LOGMESSAGE_H
 #define LOGMESSAGE_H
 
@@ -9,6 +16,14 @@
 #include <vector>
 
 
+/**
+  Append a message string with any single element.
+  @param message initial message string.
+  @param t further element to merge to the message.
+  @return message the updated message
+
+  appendMessage("nono ", 1) -> "nono 1"
+*/
 template <typename T>
 std::string& appendMessage(std::string& message, const T& t)
 {
@@ -18,7 +33,14 @@ std::string& appendMessage(std::string& message, const T& t)
   return message;
 }
 
+/**
+  Append a message string with any 1D-vector of elements.
+  @param message initial message string.
+  @param v further vector of elements to merge to the message.
+  @return message the updated message
 
+  appendMessage("nono ", [1, 2]) -> "nono 1 2"
+*/
 template <typename T>
 std::string& appendMessage(std::string& message, const std::vector<T>& v)
 {
@@ -31,7 +53,16 @@ std::string& appendMessage(std::string& message, const std::vector<T>& v)
   return message;
 }
 
+/**
+  Append a message string with any 2D-vector of elements.
+  @param message initial message string.
+  @param m further vector of elements to merge to the message.
+  @return message the updated message
 
+  appendMessage("nono ", [[1, 2],  -> "nono
+                          [3, 4]])     1 2
+                                       3 4"
+*/
 template <typename T>
 std::string& appendMessage(std::string& message,
                            const std::vector<std::vector<T>>& m)
@@ -45,7 +76,20 @@ std::string& appendMessage(std::string& message,
   return message;
 }
 
+/**
+  Append a message string with any 3D-vector of elements.
+  @param message initial message string.
+  @param m further vector of elements to merge to the message.
+  @return message the updated message
 
+  appendMessage("nono ", [[[1, 2],      "nono
+                           [3, 4]],  ->  1 2
+                          [[5, 6],       3 4
+                           [7, 8]]])
+                                         5 6
+                                         7 8
+                                         "
+*/
 template <typename T>
 std::string& appendMessage(std::string& message,
                            const std::vector<std::vector<std::vector<T>>>& m)
@@ -59,7 +103,17 @@ std::string& appendMessage(std::string& message,
   return message;
 }
 
+/**
+  Append a message string with any list of elements or vectors as set before.
+  @param message initial message string.
+  @param t further element or vector of elements to merge to the message.
+  @param t2 further element or vector of elements to merge to the message.
+  @return message the updated message
 
+  appendMessage("nono ", 1, [[2, 3], [4, 5]]) -> "nono 1
+                                                  2 3
+                                                  4 5"
+*/
 template <class T, class ...Types>
 std::string& appendMessage(std::string& message, const T& t,
                            const Types&... t2)
@@ -70,63 +124,129 @@ std::string& appendMessage(std::string& message, const T& t,
 }
 
 
+/**
+  @class LogMessage
+  Holds a message string with a given importance, compared to a verbosity level.
+*/
 class LogMessage
 {
 protected:
-  std::string m_message;
-  int m_importance, m_verbosity;
+  std::string m_message;  // Log message.
+  int m_importance, m_verbosity;  // message importance and verbosity level.
 
 public:
+  /**
+    LogMessage constructor.
+    @param importance importance level of the message.
+    @param verbosity verbosity level.
+    @param t1 content of the message.
+    @param t2 any further contents of the message.
+  */
   template <typename T, typename... Types>
   LogMessage(int importance, int verbosity, const T& t1, const Types&... t2) :
   m_importance(importance), m_verbosity(verbosity)
   {
+    // Fill the message string only if verbosity tolerates its importance level.
     if (m_importance > m_verbosity)
     {
       appendMessage(m_message, t1, t2...);
     }
   }
 
+  /**
+    LogMessage destructor.
+  */
   ~LogMessage();
 
+  /**
+    Shows the message only if verbosity tolerates its importance level.
+  */
   void show() const;
 };
 
 
+/**
+  @class DebugMessage
+  LogMessage with importance level 1.
+*/
 class DebugMessage: public LogMessage
 {
 public:
+  /**
+    DebugMessage constructor.
+    @param verbosity verbosity level.
+    @param t1 content of the message.
+    @param t2 any further contents of the message.
+  */
   template <typename T, typename... Types>
   DebugMessage(int verbosity, const T& t1, const Types&... t2) :
   LogMessage(1, verbosity, t1, t2...) {}
 };
 
 
+/**
+  @class InfoMessage
+  LogMessage with importance level 2.
+*/
 class InfoMessage: public LogMessage
 {
 public:
+  /**
+    InfoMessage constructor.
+    @param verbosity verbosity level.
+    @param t1 content of the message.
+    @param t2 any further contents of the message.
+  */
   template <typename T, typename... Types>
   InfoMessage(int verbosity, const T& t1, const Types&... t2) :
   LogMessage(2, verbosity, t1, t2...) {}
 };
 
 
+/**
+  @class WarningMessage
+  LogMessage with importance level 3.
+*/
 class WarningMessage: public LogMessage
 {
 public:
+  /**
+    WarningMessage constructor.
+    @param verbosity verbosity level.
+    @param t1 content of the message.
+    @param t2 any further contents of the message.
+  */
   template <typename T, typename... Types>
   WarningMessage(int verbosity, const T& t1, const Types&... t2) :
   LogMessage(3, verbosity, t1, t2...) {}
+
+  /**
+    Shows the message only if verbosity tolerates its importance level.
+  */
   void show() const;
 };
 
 
+/**
+  @class ErrorMessage
+  LogMessage with importance level 4.
+*/
 class ErrorMessage: public LogMessage
 {
 public:
+  /**
+    ErrorMessage constructor.
+    @param verbosity verbosity level.
+    @param t1 content of the message.
+    @param t2 any further contents of the message.
+  */
   template <typename T, typename... Types>
   ErrorMessage(int verbosity, const T& t1, const Types&... t2) :
   LogMessage(4, verbosity, t1, t2...) {}
+
+  /**
+    Shows the message only if verbosity tolerates its importance level.
+  */
   void show() const;
 };
 
--- src/Logger.h
+++ src/Logger.h
@@ -1,3 +1,10 @@
+/**
+  @file Logger.h
+  @author Guillaume dupouy
+  @version 1.0
+  @date 2020/09/15
+*/
+
 #ifndef LOGGER_H
 #define LOGGER_H
 
@@ -5,52 +12,101 @@
 #include "LogMessage.h"
 
 
+/**
+  @class Logger
+  Manage output streams according to verbosity level.
+  A Logger stores a verbosity level (0: shows any message,
+                                     1: shows any message except debug,
+                                     2: shows only warning and error messages,
+                                     3: shows only error messages,
+                                     4+: mute).
+  It shows LogMessage instances, or ingnores it, depending on the verbosity
+  level.
+*/
 class Logger
 {
 private:
-  int m_verbosity;
+  int m_verbosity;  // verbosity level
 
 public:
+	/**
+    Logger constructor.
+    Verbosity default to 0 (shows any LogMessage)
+	*/
   Logger(int verbosity=0);
+
+  /**
+    Logger destructor
+	*/
   ~Logger();
+
+  /**
+    Sets verbosity level of the Logger instance.
+    @param verbosity verbosity level.
+	*/
   void configure(int verbosity);
 
+  /**
+    Fires a DebugMessage instance.
+    @param t1 message content.
+    @param t2 message content.
+	*/
   template <typename T, typename... Types>
   void debug(const T& t1, const Types&... t2) const
   {
+    // omp critical to avoid simultaneous couts when multithreading.
     #pragma omp critical
     {
-      DebugMessage logMessage(m_verbosity, //"[DEBUG] : ",
-                              t1, t2...);
-      logMessage.show();
+      DebugMessage logMessage(m_verbosity, t1, t2...);
+      logMessage.show();  // Fires message if verbosity <= 0.
     }
   }
 
+  /**
+    Fires a InfoMessage instance.
+    @param t1 message content.
+    @param t2 message content.
+	*/
   template <typename T, typename... Types>
   void info(const T& t1, const Types&... t2) const
   {
+    // omp critical to avoid simultaneous couts when multithreading.
     #pragma omp critical
     {
       InfoMessage logMessage(m_verbosity, t1, t2...);
-      logMessage.show();
+      logMessage.show();  // Fires message if verbosity <= 1.
     }
   }
 
+  /**
+    Fires a WarningMessage instance.
+    @param t1 message content.
+    @param t2 message content.
+	*/
   template <typename T, typename... Types>
   void warning(const T& t1, const Types&... t2) const
   {
+    // Don't need omp critical there: an exception while multithreading will
+    // crash R anyway...
     {
       WarningMessage logMessage(m_verbosity, "[WARNING] : ", t1, t2...);
-      logMessage.show();
+      logMessage.show();  // Fires message if verbosity <= 2.
     }
   }
 
+  /**
+    Fires a ErrorMessage instance.
+    @param t1 message content.
+    @param t2 message content.
+	*/
   template <typename T, typename... Types>
   void error(const T& t1, const Types&... t2) const
   {
+    // Don't need omp critical there: an exception while multithreading will
+    // crash R anyway...
     {
       ErrorMessage logMessage(m_verbosity, "[ERROR] : ", t1, t2...);
-      logMessage.show();
+      logMessage.show();  // Fires message if verbosity <= 3.
     }
   }
 };
