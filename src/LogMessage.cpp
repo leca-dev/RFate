dedd28979e7523b58bbbd1f002078940d866dc03
MayaGueguen <maya.gueguen@gmail.com>
2021-02-11 10:52:08 +0100

    Add copyright to C++ files

--- src/LogMessage.cpp
+++ src/LogMessage.cpp
@@ -1,3 +1,25 @@
+/*----------------------------------------------------------------------------
+ *	
+ *	Copyright (C) 2021 Isabelle Boulangeat, Damien Georges, Maya Guéguen,
+ *	Wilfried Thuiller
+ *	
+ *	This file is part of FATE.
+ *	
+ *	FATE is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	FATE is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with FATE. If not, see <https://www.gnu.org/licenses/>.
+ *	
+ --------------------------------------------------------------------------*/
+
 #include "LogMessage.h"
 
 using namespace std;
