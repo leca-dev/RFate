70f5e1703a0630594260ee1097aa4cd7985de7cc
Maxime Delprat <maxime-delprat@orange.fr>
2022-06-08 11:54:47 +0200

    minor corrections

--- R/UTILS.train_RF_habitat.R
+++ R/UTILS.train_RF_habitat.R
@@ -204,9 +204,9 @@ train_RF_habitat = function(releves.PFG
   } else if (perStrata == FALSE & !is.null(hab.obs.RF)) {
     mat.PFG.agg = aggregate(coverage ~ site + PFG, data = releves.PFG, FUN = "sum")
     mat.PFG.agg$strata = "A"
-  } else if (perStrata = TRUE & is.null(hab.obs.RF)) {
+  } else if (perStrata == TRUE & is.null(hab.obs.RF)) {
     mat.PFG.agg = aggregate(coverage ~ site + PFG + strata + code.habitat, data = releves.PFG, FUN = "sum")
-  } else if (perStata = FALSE & is.null(hab.obs.RF)) {
+  } else if (perStrata == FALSE & is.null(hab.obs.RF)) {
     mat.PFG.agg = aggregate(coverage ~ site + PFG + code.habitat, data = releves.PFG, FUN = "sum")
     mat.PFG.agg$strata = "A"
   }
